92f8f65524979c862055bc0aa87b4a73be578675
Abtinz <Abtinzandi@gmail.com>
2026-03-16 20:33:26 -0400

    OpenAI and Ollama chat functions

--- LangChain/udemy_course/hello_world/main.py
+++ LangChain/udemy_course/hello_world/main.py
@@ -2,19 +2,42 @@ from prompt import prompt
 from knowledgebase import information
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
-def main():
+def openAIChat(prompt):
+
     llm = ChatOpenAI(
         model="gpt-5", 
         temperature=0
-    )
+    ) 
 
     chain = prompt | llm
-
     response = chain.invoke({"information": information})
     print(response.content)
+
+def ollamaChat(prompt):
+
+    llm = ChatOllama(
+        model="gemma3:270m", 
+        temperature=0
+    ) 
+
+    chain = prompt | llm
+    response = chain.invoke({"information": information})
+
+    print(response.content) 
+
+def main():
+    
+    print("OpenAI Chat:")
+    openAIChat(prompt)
+
+    print("\nOllama Chat:")
+    ollamaChat(prompt)
+
+
 
 if __name__ == "__main__":
     main()
