671c3fc2a0b8365f9c05b5045744ae592331bc61
Abtinz <Abtinzandi@gmail.com>
2026-03-18 17:37:09 -0400

    first simple agent for the course

--- LangChain/udemy_course/search-agent/main.py
+++ LangChain/udemy_course/search-agent/main.py
@@ -1,1 +1,38 @@
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke(
+        {"messages": HumanMessage(content="What is the weather in Tokyo?")}
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
