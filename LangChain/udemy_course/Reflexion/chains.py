1dd70c7cf8eb2369af19f9d09acd18d9f2606fb2
Abtinz <Abtinzandi@gmail.com>
2026-04-18 19:26:48 -0400

    Add Reflexion main flow and tool executor

--- LangChain/udemy_course/Reflexion/chains.py
+++ LangChain/udemy_course/Reflexion/chains.py
@@ -32,19 +32,3 @@ first_responder = first_responder_prompt_template | llm.bind_tools(
 revisor = actor_prompt_template.partial(
     first_instruction=revise_instructions
 ) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
-
-
-if __name__ == "__main__":
-    human_message = HumanMessage(
-        content="Write about AI-Powered SOC / autonomous soc  problem domain,"
-        " list startups that do that and raised capital."
-    )
-
-    chain = (
-        first_responder_prompt_template
-        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
-        | parser_pydantic
-    )
-
-    res = chain.invoke(input={"messages": [human_message]})
-    print(res)
