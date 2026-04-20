f4feb898168ad477c492b16791013d5b9df40278
Abtinz <Abtinzandi@gmail.com>
2026-04-20 16:24:28 -0400

    feat: add adaptive rag question router

--- LangChain/udemy_course/Agentic-RAGs/adaptive-rag/graph/graph.py
+++ LangChain/udemy_course/Agentic-RAGs/adaptive-rag/graph/graph.py
@@ -3,6 +3,7 @@ from langgraph.graph import END, StateGraph
 
 from graph.chains.answer_grader import answer_grader
 from graph.chains.hallucination_grader import hallucination_grader
+from graph.chains.router import RouteQuery, question_router
 from graph.consts import GENERATE, GRADE_DOCUMENTS, RETRIEVE, WEBSEARCH
 from graph.nodes import generate, grade_documents, retrieve, web_search
 from graph.state import GraphState
@@ -51,6 +52,17 @@ def grade_generation_grounded_in_documents_and_question(state: GraphState) -> st
     return "not supported"
 
 
+def route_question(state: GraphState) -> str:
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    source: RouteQuery = question_router.invoke({"question": question})
+    if source.datasource == WEBSEARCH:
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return WEBSEARCH
+    print("---ROUTE QUESTION TO RAG---")
+    return RETRIEVE
+
+
 workflow = StateGraph(GraphState)
 # Add nodes to the graph and define the edges between them
 workflow.add_node(RETRIEVE, retrieve)
@@ -60,7 +72,13 @@ workflow.add_node(WEBSEARCH, web_search)
 
 # Define the edges between the nodes
 # including conditional edges based on the output of the GRADE_DOCUMENTS node
-workflow.set_entry_point(RETRIEVE)
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        WEBSEARCH: WEBSEARCH,
+        RETRIEVE: RETRIEVE,
+    },
+)
 workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS)
 workflow.add_conditional_edges(
     GRADE_DOCUMENTS,
--- LangChain/udemy_course/Agentic-RAGs/adaptive-rag/main.py
+++ LangChain/udemy_course/Agentic-RAGs/adaptive-rag/main.py
@@ -5,4 +5,4 @@ from graph.graph import app
 
 if __name__ == "__main__":
     print("Hello Advanced RAG")
-    print(app.invoke(input={"question": "what is agent memory?"}))
+    print(app.invoke(input={"question": "agent memory?"}))
